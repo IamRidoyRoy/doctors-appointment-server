836aaf178f7f3932aa322a53e35eab68cc321c81
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-27 23:11:50 +0600

    Assign jwt token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -105,8 +107,8 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send(result);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
         })
 
     }
